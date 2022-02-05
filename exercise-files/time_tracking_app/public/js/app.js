47b4bea52bbba0dfa3cc2234d5731f73e0d7aa1d
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-02-05 14:35:23 -0600

    Initial changes

--- exercise-files/time_tracking_app/public/js/app.js
+++ exercise-files/time_tracking_app/public/js/app.js
@@ -1,1 +1,39 @@
 
+class Timer extends React.Component {
+    render() {
+      const { title, project } = this.props;
+      return (
+        <div className='ui centered card'>
+          <div className='content'>
+            <div className='header'>
+              {title}
+            </div>
+            <div className='meta'>
+              {project}
+            </div>
+            <div className='center aligned description'>
+              <h2>
+                12:20
+              </h2>
+            </div>
+            <div className='extra content'>
+              <span className='right floated edit icon'>
+                <i className='edit icon' />
+              </span>
+              <span className='right floated trash icon'>
+                <i className='trash icon' />
+              </span>
+            </div>
+          </div>
+          <div className='ui bottom attached blue basic button'>
+            Start
+          </div>
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <Timer />,
+    document.getElementById('content')
+  );
